8f88ece5c770d540e3ef002ec12b8b253c7cfd87
Iwtrgw <Iwtrgw@Outlook.com>
2018-08-25 03:23:11 +0000

    根据话题回复数排序

--- app/Filters/ThreadsFilters.php
+++ app/Filters/ThreadsFilters.php
@@ -7,12 +7,22 @@ use Illuminate\Http\Request;
 
 class ThreadsFilters extends Filters
 {
-	protected $filters = ['by'];
+	protected $filters = ['by','popularity'];
 
 	protected function by($username)
 	{
 		$user = User::where('name',$username)->firstOrFail();
 
 		return $this->builder->where('user_id',$user->id);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function popularity()
+	{
+		$this->builder->getQuery()->orders = [];
+
+		return $this->builder->orderBy('replies_count','desc');
 	}
 }
