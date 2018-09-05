7592e3f27e32a0f95e9ce1026d4e5dfb66d3f389
Iwtrgw <Iwtrgw@Outlook.com>
2018-09-05 03:19:11 +0000

    优化订阅通知部分代码

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -23,22 +23,17 @@ class NotificationsTest extends TestCase
     public function test_a_notifcation_is_prepared_when_a_subscribed_thread_receives_a_new_reply_that_is_not_by_the_current_user()
     {
     	$thread = create('App\Thread')->subscribe();
-
-    	$this->assertCount(0,auth()->user()->notifications);
-
-    	$thread->addReply([
-    		'user_id' => auth()->id(),
-    		'body' => 'Some reply here.'
-    	]);
-
-    	$this->assertCount(0,auth()->user()->fresh()->notifications);
-
-    	$thread->addReply([
-    		'user_id' => create('App\User')->id,
-    		'body' => 'Some reply here'
-    	]);
-
-    	$this->assertCount(1,auth()->user()->fresh()->notifications);
+        $this->assertCount(0, auth()->user()->notifications);
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here'
+        ]);
+        $this->assertCount(0, auth()->user()->fresh()->notifications);
+        $thread->addReply([
+            'user_id' => create('App\User')->id,
+            'body' => 'Some reply here'
+        ]);
+        $this->assertCount(1, auth()->user()->fresh()->notifications);
 
     }
 
