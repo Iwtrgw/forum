aaeee4d286f5f1d8fd0710fb2c2dbac6b037e201
Iwtrgw <Iwtrgw@Outlook.com>
2018-09-11 08:37:36 +0000

    添加热闹话题，使用 Redis 作缓存

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -6,6 +6,7 @@ use App\Thread;
 use App\Channel;
 use Illuminate\Http\Request;
 use App\Filters\ThreadsFilters;
+use Illuminate\Support\Facades\Redis;
 
 class ThreadController extends Controller
 {
@@ -28,7 +29,9 @@ class ThreadController extends Controller
             return $threads;
         }
 
-        return view('threads.index',compact('threads'));
+        $trending = array_map('json_decode',Redis::zrevrange('trending_threads',0,4));
+
+        return view('threads.index',compact('threads','trending'));
     }
 
     protected function getThreads(Channel $channel,ThreadsFilters $filters)
@@ -89,6 +92,11 @@ class ThreadController extends Controller
         if (auth()->check()) {
             auth()->user()->read($thread);
         }
+
+        Redis::zincrby('trending_threads',1,json_encode([
+            'title' => $thread->title,
+            'path' => $thread->path()
+        ]));
         
         return view('threads.show',compact('thread'));
     }
